e4d6a9ff24a9df1ed5e529a9097dcc933aad7b40
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-23 16:01:31 -0300

    melhoria no router para usar o controller

--- src/personagem/personagem.router.js
+++ src/personagem/personagem.router.js
@@ -1,15 +1,15 @@
 const express=require('express')
-const { readAll, readById, create, updateById, deleleById } = require('./personagem.controller')
+const controller = require('./personagem.controller')
 const router = express.Router()
 
 function teste(req,res){
     res.send('ok')
 }
 
-router.get('/', readAll)
-router.get('/:id', readById)
-router.post('/', create)
-router.put('/:id', updateById)
-router.delete('/:id', deleleById)
+router.get('/', controller.readAll)
+router.get('/:id', controller.readById)
+router.post('/', controller.create)
+router.put('/:id', controller.updateById)
+router.delete('/:id', controller.deleleById)
 
 module.exports = router
