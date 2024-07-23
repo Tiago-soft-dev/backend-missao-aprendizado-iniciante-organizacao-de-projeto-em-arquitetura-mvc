b29dcb08a7f5058bf79c1e0d521521ed8d9c2c0f
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-23 16:09:25 -0300

    criação inicial do service

---
+++ src/personagem/personagem.service.js
@@ -0,0 +1,25 @@
+const { Collection } = require("mongodb")
+
+function readAll(){
+    return []
+}
+
+async function readById(){
+}
+
+function create(){
+}
+
+function updateById(){
+}
+
+function deleleById(){
+}
+
+module.exports = {
+    readAll,
+    readById,
+    create,
+    updateById,
+    deleleById
+}
